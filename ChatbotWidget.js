97f5e8460f0dd6ae5a5f2856108d7322e062c1dd
abdullahghujah <74357097+abdullahghujah@users.noreply.github.com>
2025-04-21 17:02:02 +0300

    Create ChatbotWidget.js

---
+++ ChatbotWidget.js
@@ -0,0 +1,14 @@
+  (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '67a46c66a2b7fc2b71d1fcf9' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
+          }
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
