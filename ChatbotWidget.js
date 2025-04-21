223b8f367c0d3df29ffa0511dd744e8ba8b64e29
abdullahghujah <74357097+abdullahghujah@users.noreply.github.com>
2025-04-21 20:41:25 +0300

    add logs ChatbotWidget.js

--- ChatbotWidget.js
+++ ChatbotWidget.js
@@ -1,6 +1,8 @@
 <script type="text/javascript">
   (function(d, t) {
       var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    console.log("V=" + v.outerHTML);
+    console.log("S=" + s.outerHTML);
       v.onload = function() {
         window.voiceflow.chat.load({
           verify: { projectID: '67a46c66a2b7fc2b71d1fcf9' },
@@ -12,5 +14,6 @@
         });
       }
       v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+    console.log("Parent=" + s.parentNode.outerHTML);
   })(document, 'script');
 </script>
