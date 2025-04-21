fdb4a3cee0f7793194c306de327cfef1e90c2447
abdullahghujah <74357097+abdullahghujah@users.noreply.github.com>
2025-04-21 18:15:35 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
 (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '67a46c66a2b7fc2b71d1fcf9' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
+          }
+        });
+      };
       console.log("Hi");
   })(document, 'script');
